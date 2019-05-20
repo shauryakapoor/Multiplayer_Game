3c82c75883a306cbddebfa71075d823be5ae32e5
Shaurya Kapoor <shaunkapoor170@gmail.com>
2019-05-20 15:44:47 -0700

    Just add the basic thesis of the project

---
+++ Python_game/game.py
@@ -0,0 +1,10 @@
+#Theory behind the project 
+#Online game development with Python 
+#sockets and networking to connect clients and server to connect what is know
+#as client or server or multiple clients to a server where they can send and
+#share information where they can send and share information and therefore 
+#we can create an online game. Massive amounts of info to the server and back
+#to the client. Start simply by getting eveything working on our local
+#network and then go ahead and deploy it on an external server which will
+#allow us to play from anywhere in the world and not just against people on
+#our local network
