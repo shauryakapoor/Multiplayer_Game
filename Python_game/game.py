1087f68ef255e0b0904f5d86ed1a396ab21bf01d
Shaurya Kapoor <shaunkapoor170@gmail.com>
2019-05-20 16:06:13 -0700

    Added more comments

--- Python_game/game.py
+++ Python_game/game.py
@@ -1,10 +1,18 @@
-#Theory behind the project 
-#Online game development with Python 
-#sockets and networking to connect clients and server to connect what is know
-#as client or server or multiple clients to a server where they can send and
-#share information where they can send and share information and therefore 
-#we can create an online game. Massive amounts of info to the server and back
-#to the client. Start simply by getting eveything working on our local
-#network and then go ahead and deploy it on an external server which will
-#allow us to play from anywhere in the world and not just against people on
-#our local network
+'''Theory behind the project 
+Online game development with Python 
+sockets and networking to connect clients and server to connect what is know
+as client or server or multiple clients to a server where they can send and
+share information where they can send and share information and therefore 
+we can create an online game. Massive amounts of info to the server and back
+to the client. Start simply by getting eveything working on our local
+network and then go ahead and deploy it on an external server which will
+allow us to play from anywhere in the world and not just against people on
+our local network. The way any online game work is we have multiple 
+connecting to one main location known as the server. Command line will be
+sending and receiving information and this is how an online game works. 
+If ever an online game says, waiting for server --> Then it is doing exactly
+that. Waiting to get a connection from the server and then grab information
+from that. No frameworks will be used. The only thing that might be used
+that is external is pygame just to create some basic graphics.
+'''
+
